b8202f0c7c483acccf146d4e1d2e017cc2a2524e
huynhnam249 <67179307+huynhnam249@users.noreply.github.com>
2022-12-07 09:15:42 +0700

    change redirect in checkAuth

--- services/checkAuth.js
+++ services/checkAuth.js
@@ -1,9 +1,9 @@
 const checkAuth = (req, res, next) => {
-    const redirectURL = `https://${req.headers.host}`
-    console.log(redirectURL, req.secure, req.headers.host)
+    // const redirectURL = `https://${req.headers.host}`
+    // console.log(redirectURL, req.secure, req.headers.host)
     if (req.session.user === null || req.session.user === undefined) {
         return res.redirect(
-            `https://identity-provider.onrender.com/cas/login?serviceURL=${redirectURL}`
+            `https://identity-provider.onrender.com/cas/login?serviceURL=https://${req.headers.host}`
         )
     }
     next()
